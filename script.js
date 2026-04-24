b15799ce922bf3f4b3448e934aeb7a9d25db04db
Danielthejavascriptmaster <onewhoknocksthe0.com>
2026-04-24 16:17:54 +0200

    fixing image slider

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
-console.log("Script loaded successfully!");
+const leftArrow = document.getElementById("left-arrow");
+const rightArrow = document.getElementById("right-arrow");
 
-const images = [
-  "./assets/images/lamine-yamal-wearing-f50-pink.jpg",
-  "./assets/images/lamine-yamal-juggling-ball-with-pink-f50.jpg",
-  "./assets/images/lamine-yamal-smiling-with-red-shades-pink-room.jpg",
-  "./assets/images/lamine-yamal-pink-f50.avif",
-  "./assets/images/lamine-yamal-pink-f50-above-view.avif",
+const heroImage = document.querySelector(".hero-container img");
+
+let images = [
+    "./assets/images/lamine-yamal-wearing-f50-pink.jpg",
+    "./assets/images/lamine-yamal-smiling-with-red-shades-pink-room.jpg",
+    "./assets/images/lamine-yamal-juggling-ball-with-pink-f50.jpg",
+    "./assets/images/lamine-yamal-pink-f50.avif",
+    "./assets/images/lamine-yamal-pink-f50-above-view.avif"
 ];
 
 let currentIndex = 0;
 
-const hero = document.getElementById("hero-image");
-const leftBtn = document.getElementById("left-arrow");
-const rightBtn = document.getElementById("right-arrow");
+function showSlide(index) {
+    heroImage.src = images[index];
+}
 
+function nextSlide() {
+    currentIndex++;
+    if (currentIndex >= images.length) {
+        currentIndex = 0;
+    }
+    showSlide(currentIndex);
+}
+
+function prevSlide() {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = images.length - 1;
+    }
+    showSlide(currentIndex);
+}
+
+
+
+rightArrow.addEventListener("click", nextSlide);
+leftArrow.addEventListener("click", prevSlide);

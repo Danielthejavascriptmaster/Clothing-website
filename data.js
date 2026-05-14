450e06b0d89da7464ec7ab37e495d88ff0e0a514
Danielthejavascriptmaster <onewhoknocksthe0.com>
2026-05-14 22:29:58 +0200

    navbar/filter section worked on + some small fixes

--- data.js
+++ data.js
@@ -1,14 +1,67 @@
-let shoes = [
+let F50 = [
     {
         name: "Adidas",
-        version: "Club",
-        price: 35,
+        type: "football shoe",
+        version: "F50",
+        varient: "club",
+        price: 55,
         laceless: false,
         colorWay: "Born For Goals",
-        description: "good boots for beginner that wanna test or that are starting with fotball",
+        color: ["lime", "white"],
+        description: "good boots for beginner that wanna test or that are starting with fotball.",
         image: "",
-        studType: "AG"
+        studType: "AG",
+        rating: "★★★★☆ (128)"
+    },
+
+    {
+        name: "Adidas",
+        type: "football shoe",
+        version: "F50",
+        varient: "pro",
+        price: 120,
+        laceless: false,
+        colorWay: "Born For Goals",
+        color: ["lime", "white"],
+        description: "good boots for experienced players that play casually and want to have a good pair of boots.",
+        image: "",
+        studType: "AG",
+        rating: "★★★★★ (95)"
+    },
+
+    {
+        name: "Adidas",
+        type: "football shoe",
+        version: "F50",
+        varient: "elite",
+        price: 300,
+        laceless: false,
+        colorWay: "Born For Goals",
+        color: ["lime", "white"],
+        description: "Advanced boot for experienced players that play competitively and want to have the best pair of boots.",
+        image: "",
+        studType: "AG",
+        rating: "★★★★★ (305)"
+    },
+
+    {
+        name: "Adidas",
+        type: "football shoe",
+        version: "F50",
+        varient: "club",
+        price: 60,
+        laceless: true,
+        colorWay: "Born For Goals",
+        color: ["lime", "white"],
+        description: "good boots for beginner that wanna test or that are starting with fotball and don't like laces.",
+        image: "",
+        studType: "AG",
+        rating: "★★★★★ (305)"
     }
+
+    
+
+
 
 ];
 
--- function.js
+++ function.js
@@ -1,44 +1,22 @@
-const filterBtn = document.getElementById("filter-close-btn");
-const filterSection = document.querySelector(".filter-section");
-const productSection = document.querySelector(".product-section");
+const filterContent = document.querySelectorAll(".filter-content");
+const filterTitle = document.querySelectorAll(".filter-title");
+
+const filterTitles = document.querySelectorAll(".filter-title");
 
 
-filterBtn.addEventListener("click", function () {
 
+filterTitles.forEach(title => {
+  title.addEventListener("click", () => {
     
-    filterSection.style.display = "none";
+    const content = title.nextElementSibling;
 
-    
-    productSection.style.marginLeft = "0px";
 
-    
-    productSection.style.width = "100%";
-
-    
-    const openBtn = document.createElement("button");
-    openBtn.textContent = ">";
-
-    openBtn.style.position = "fixed";
-    openBtn.style.top = "200px";
-    openBtn.style.left = "20px";
-    openBtn.style.padding = "10px";
-    openBtn.style.backgroundColor = "white";
-    openBtn.style.borderRadius = "8px";
-    openBtn.style.cursor = "pointer";
-    openBtn.style.zIndex = "999";
-
-    document.body.appendChild(openBtn);
-
-   
-    openBtn.addEventListener("click", function () {
-
-        
-        filterSection.style.display = "flex";
-
-        
-        productSection.style.marginLeft = "220px";
-        productSection.style.width = "auto";
-
-        openBtn.remove();
-    });
+    if(content.style.display === "flex"){
+        content.style.display = "none"
+    } else{
+        content.style.display = "flex"
+    }
+  });
 });
+
+

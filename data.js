7b3615f37aa1875f6dbcab8d3ae874220f4303b6
Danielthejavascriptmaster <onewhoknocksthe0.com>
2026-05-15 12:23:17 +0200

    data.js progress (colorway seperation for cleaner structure)

--- data.js
+++ data.js
@@ -1,18 +1,5 @@
-const products = [
-    
+const f50ColorWays = [
     {
-        id: 1,
-        name: "Adidas f50",
-        brand: "Adidas",
-        categrory: "cleats",
-        versions: [
-            {
-                name: "F50 club",
-                price: 50,
-                laceless: false,
-                studTypes: ["FG", "AG", "SG"],
-                colorWays: [
-                        {
                             colorWay: "Born For Goals",
                             color:["lime", "black", "white"],
                             hex: ["#d2ff5f", "#000000", "#ffffff"],
@@ -42,20 +29,35 @@ const products = [
 
                         {
                             colorWay: "Road To Glory",
-                            color: ["gold", "black", "white", "lightpurple"],
+                            color: ["gold", "black", "white", "lavender"],
                             hex: ["#FFD700", "#000000", "#ffffff", "#d8b9f3"],
                             image: "assets/images/f50-road-to-glory-elite.webp"
                         },
 
                         {
                             colorWay: "Mystic Glory",
-                            color: ["lime", "pink", "lightpurple" ],
-                            hex: ["#FFFF00", "#f300a6", "#d8b9f3"],
+                            color: ["lime", "pink", "lavender" ],
+                            hex: ["#d2ff5f", "#f300a6", "#d8b9f3"],
                             image: "assets/images/f50-mystic-glory-elite.webp"
                         },
-                    ],
+]
 
-                
+
+const products = [
+    
+    {
+        id: 1,
+        name: "Adidas f50",
+        brand: "Adidas",
+        category: "cleats",
+        versions: [
+
+            {
+                name: "F50 club",
+                price: 50,
+                laceless: false,
+                studTypes: ["FG", "AG", "SG"], 
+                colorWays: f50ColorWays
             },
 
             { 
@@ -63,49 +65,7 @@ const products = [
                 price:120,
                 laceless: false,
                 studTypes: ["FG", "AG", "SG"],
-                colorWays: [
-                        {
-                            colorWay: "Born For Goals",
-                            color:["lime", "black", "white"],
-                            hex: ["#d2ff5f", "#000000", "#ffffff"],
-                            image: "assets/images/f50-born-for-goals-elite.webp"
-                        },
-
-                        {
-                            colorWay: "Pure Victory",
-                            color: ["white", "red", "lime"],
-                            hex: ["#ffffff", "#ff0000", "#d2ff5f"],
-                            image: "assets/images/f50-pure-victory-elite.webp"
-                        },
-
-                        {
-                            colorWay: "Stealth Victory",
-                            color: ["black", "red", "white", "grey"],
-                            hex: ["#000000", "#ff0000", "#ffffff", "#808080"],
-                            image: "assets/images/f50-stealth-victory-elite.webp"
-                        },
-
-                        {
-                            colorWay: "Coral blaze",
-                            color: ["blue", "coral", "white"],
-                            hex: ["#0000ff", "#ff7f00", "#ffffff"],
-                            image: "assets/images/f50-coral-blaze-elite.webp"
-                        },
-
-                        {
-                            colorWay: "Road To Glory",
-                            color: ["gold", "black", "white", "lightpurple"],
-                            hex: ["#FFD700", "#000000", "#ffffff", "#d8b9f3"],
-                            image: "assets/images/f50-road-to-glory-elite.webp"
-                        },
-
-                        {
-                            colorWay: "Mystic Glory",
-                            color: ["lime", "pink", "lightpurple" ],
-                            hex: ["#FFFF00", "#f300a6", "#d8b9f3"],
-                            image: "assets/images/f50-mystic-glory-elite.webp"
-                        },
-                    ],
+                colorWays: f50ColorWays
             },
 
             {
@@ -113,53 +73,12 @@ const products = [
                 price: 310,
                 laceless: false,
                 studTypes: ["FG", "AG", "SG"],
-                colorWays: [
-                        {
-                            colorWay: "Born For Goals",
-                            color:["lime", "black", "white"],
-                            hex: ["#d2ff5f", "#000000", "#ffffff"],
-                            image: "assets/images/f50-born-for-goals-elite.webp"
-                        },
+                colorWays: f50ColorWays
+            },
 
-                        {
-                            colorWay: "Pure Victory",
-                            color: ["white", "red", "lime"],
-                            hex: ["#ffffff", "#ff0000", "#d2ff5f"],
-                            image: "assets/images/f50-pure-victory-elite.webp"
-                        },
-
-                        {
-                            colorWay: "Stealth Victory",
-                            color: ["black", "red", "white", "grey"],
-                            hex: ["#000000", "#ff0000", "#ffffff", "#808080"],
-                            image: "assets/images/f50-stealth-victory-elite.webp"
-                        },
-
-                        {
-                            colorWay: "Coral blaze",
-                            color: ["blue", "coral", "white"],
-                            hex: ["#0000ff", "#ff7f00", "#ffffff"],
-                            image: "assets/images/f50-coral-blaze-elite.webp"
-                        },
-
-                        {
-                            colorWay: "Road To Glory",
-                            color: ["gold", "black", "white", "lightpurple"],
-                            hex: ["#FFD700", "#000000", "#ffffff", "#d8b9f3"],
-                            image: "assets/images/f50-road-to-glory-elite.webp"
-                        },
-
-                        {
-                            colorWay: "Mystic Glory",
-                            color: ["lime", "pink", "lightpurple" ],
-                            hex: ["#FFFF00", "#f300a6", "#d8b9f3"],
-                            image: "assets/images/f50-mystic-glory-elite.webp"
-                        },
-                    ],
-                },
-            ],
-        },
-    ];
+        ],
+    },
+];
 
 
 //*!--------------------next sectiond----------------------
